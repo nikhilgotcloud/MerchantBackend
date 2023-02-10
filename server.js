7a9a036638a029b8419ae1be288e99957279a2d8
Nikhil Anand <112159766+nikhilgotcloud@users.noreply.github.com>
2023-02-10 13:08:19 +0530

    second

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-  origin:["http://localhost:3000/","https://cartplus.netlify.app/"],
+  origin:["http://localhost:3000/","https://cartplus.vercel.app"],
   credentials: true,
 }));
 
