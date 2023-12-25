0f91aae7945b7d1d5af54311ae702cfd1e6e30bd
nikhilgotcloud <nikhilanand73@gmail.com>
2023-12-25 11:21:47 +0530

    changeeessz

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const contactRoute = require("./routes/contactRoute");
-const errorHandler = require("./middleware/errorMiddleware");
+const errorHandler =require("./middleWare/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
